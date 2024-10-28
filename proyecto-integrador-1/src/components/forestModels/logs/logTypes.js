f5f69f1c18150593626d462ab794a00a06cc3720
Juan Camilo Narváez <ulvenforst@gmail.com>
2024-10-28 18:41:50 -0500

    logs uploaded

---
+++ proyecto-integrador-1/src/components/forestModels/logs/logTypes.js
@@ -0,0 +1,10 @@
+export const LOG_TYPES = [
+  {
+    modelName: "log_big_knotty.glb",
+    nodeName: "Log_big_knotty"
+  },
+  {
+    modelName: "log_big_regular.glb",
+    nodeName: "Log_big_regular"
+  },
+];
