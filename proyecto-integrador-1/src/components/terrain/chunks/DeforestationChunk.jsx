e651664a9e94092840cc360b3c3a6d11399b9109
Brayan Steven Narvez Valdes <brayan.steven.narvaez@correounivalle.edu.co>
2024-10-28 20:36:31 -0500

    coreccoin

--- proyecto-integrador-1/src/components/terrain/chunks/DeforestationChunk.jsx
+++ proyecto-integrador-1/src/components/terrain/chunks/DeforestationChunk.jsx
@@ -1,8 +1,8 @@
-import StonesBlock from '../../forestModels/stones/StonesBlock';
-import ChunkGenerator from './ChunkGenerator';
-import StumpsBlock from '../../forestModels/stumps/StumpsBlock';
-import LogsBlock from '../../forestModels/logs/LogsBlock';
-import { FLOOR_TYPES } from '../floors';
+import StonesBlock from "../../forestModels/stones/StonesBlock";
+import ChunkGenerator from "./ChunkGenerator";
+import StumpsBlock from "../../forestModels/stumps/StumpsBlock";
+import LogsBlock from "../../forestModels/logs/LogsBlock";
+import { FLOOR_TYPES } from "../floors";
 
 const DeforestationChunk = ({ position, seed = 13345 }) => {
   const blocks = [
@@ -14,8 +14,8 @@ const DeforestationChunk = ({ position, seed = 13345 }) => {
         seedOffset: 2,
         minRadius: 1.5,
         randomRotation: true,
-        maxTiltAngle: Math.PI
-      }
+        maxTiltAngle: Math.PI,
+      },
     },
     {
       Component: StumpsBlock,
@@ -23,8 +23,8 @@ const DeforestationChunk = ({ position, seed = 13345 }) => {
         n: 20,
         factor: 35,
         seedOffset: 4,
-        minRadius: 1.5
-      }
+        minRadius: 1.5,
+      },
     },
     {
       Component: StonesBlock,
@@ -32,14 +32,14 @@ const DeforestationChunk = ({ position, seed = 13345 }) => {
         n: 3,
         factor: 35,
         seedOffset: 3,
-        minRadius: 1.5
-      }
+        minRadius: 1.5,
+      },
     },
   ];
   return (
-    <ChunkGenerator 
-      position={position} 
-      seed={seed} 
+    <ChunkGenerator
+      position={position}
+      seed={seed}
       blocks={blocks}
       floor={FLOOR_TYPES.DEFORESTED}
     />
